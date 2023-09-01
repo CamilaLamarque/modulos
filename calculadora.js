4e78e27c44bb175a9d4c40ed34c663f3e12d36db
camilala <camilalamarque10@gmail.com>
2023-09-01 09:13:50 -0300

    versao com mensagem

--- calculadora.js
+++ calculadora.js
@@ -2,6 +2,8 @@ var somar = require("./modulos/somar");
 var media = require("./modulos/media");
 var dividir = require("./modulos/dividir");
 var multiplicar= require("./modulos/multiplicar");
+var mensagem = require ("./modulos/mensagem");
+var nome = "camila";
 
 
 
@@ -9,3 +11,4 @@ console.log(somar(20,90));
 console.log(media(20,90));
 console.log(dividir(20,90))
 console.log(multiplicar(20,90))
+console.log (mensagem(nome));
