930a51deb22685d46dec22f828f4eb3f08b0b2e2
camilala <camilalamarque10@gmail.com>
2023-09-01 09:34:52 -0300

    nova mensagem

--- calculadora.js
+++ calculadora.js
@@ -3,7 +3,8 @@ var media = require("./modulos/media");
 var dividir = require("./modulos/dividir");
 var multiplicar= require("./modulos/multiplicar");
 var mensagem = require ("./modulos/mensagem");
-var nome = "camila";
+var nome = " camila";
+var msg = "oi estranho";
 
 
 
@@ -11,4 +12,4 @@ console.log(somar(20,90));
 console.log(media(20,90));
 console.log(dividir(20,90))
 console.log(multiplicar(20,90))
-console.log (mensagem(nome));
+console.log (mensagem(nome, msg));
